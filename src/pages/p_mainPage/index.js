ba0164a1e1d8dd766ba0723793101b9d40e21b16
dongshinlee <leened@naver.com>
2025-05-01 10:52:57 +0900

    save

--- src/pages/p_mainPage/index.js
+++ src/pages/p_mainPage/index.js
@@ -1,34 +1,3 @@
-/* 이미지 슬라이더 */
-function imgSlider() {
-    const imgBoxes = document.querySelectorAll('.visualSlider .imgBox');
-
-    let currentIndex = 0;
-
-    imgBoxes[currentIndex].classList.add('visible');
-    // imgBox 순환 함수
-    function showNextImage() {
-        // 다음 인덱스로 이동 (마지막 이미지일 경우 첫 번째로 돌아감)
-        const nextIndex = (currentIndex + 1) % imgBoxes.length;
-
-        // 다음 이미지에 visible 클래스 추가
-        imgBoxes[nextIndex].classList.add('visible');
-
-        // 현재 이미지에 hiding 클래스 추가
-        imgBoxes[currentIndex].classList.add('hiding');
-
-        // 0.5초 후에 현재 이미지에서 visible, hiding 클래스를 제거
-        setTimeout(() => {
-            imgBoxes[currentIndex].classList.remove('visible');
-            imgBoxes[currentIndex].classList.remove('hiding');
-            // 현재 인덱스를 다음 인덱스로 업데이트
-            currentIndex = nextIndex;
-        }, 500); // 500ms (0.5초)
-    }
-
-    // 6 초마다 showNextImage 함수 실행
-    setInterval(showNextImage, 5500);
-}
-
 // 이미지 스크롤
 function scrollImg({ itemSelector, prevButtonSelector, nextButtonSelector }) {
     const scrollItems = document.querySelectorAll(itemSelector);
@@ -36,7 +5,7 @@ function scrollImg({ itemSelector, prevButtonSelector, nextButtonSelector }) {
     const nextBtns = document.querySelectorAll(nextButtonSelector);
 
     const scrollAmount = 600;
-
+    
     scrollItems.forEach((scroll, index) => {
         const prevBtn = prevBtns[index];
         const nextBtn = nextBtns[index];
@@ -69,7 +38,7 @@ function loadGallery(galleryId) {
         .then(html => {
             contentArea.innerHTML = html;
 
-            // 새로 로드된 내용에 대해 함수 재호출
+            // 새로 로드된 내용에 대해 함수 재호출(시작)
             scrollImg({
                 itemSelector: ".bigArticleScroll",
                 prevButtonSelector: ".prevBigArticle",
@@ -83,6 +52,8 @@ function loadGallery(galleryId) {
             });
 
             heart();
+            // 새로 로드된 내용에 대해 함수 재호출(끝)
+
 
             // 새 페이지에 슬라이드 적용
             new bootstrap.Carousel(document.querySelector('#visualSlider'), {
@@ -146,11 +117,8 @@ async function applyProductInfo(num) {
     bigArticles.forEach((article, idx) => {
         // productList 48 ~ 83 큰 사진
         let product = productList.productList[(num-1) * 6 + idx + 48];
-        console.log((num-1) * 6 + idx + 48);
-        if (!product) return;
 
-        console.log(idx);
-        console.log((num-1) * 6 + idx + 48);
+        if (!product) return;
 
         let imgElement = article.querySelector('.thumb img');
         if (imgElement) {
